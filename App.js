3c7f53e15ef1c7e6bdfaaea391e9ab812ac80700
rahoolkar <batman30jan2002@gmail.com>
2026-04-14 18:57:10 +0530

    gitignore file

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import Footer from "./Footer.jsx";
+import "./RestaurantCard.css";
+import { FaStar } from "react-icons/fa";
 
 function Header() {
   return (
@@ -40,6 +42,54 @@ function Header() {
     </div>
   );
 }
+
+import React from "react";
+import "./RestaurantCard.css";
+import { FaStar } from "react-icons/fa";
+
+const RestaurantCard = ({ data }) => {
+  const {
+    name,
+    image,
+    rating,
+    time,
+    cuisine,
+    location,
+  } = data;
+
+  return (
+    <div className="restaurant-card">
+
+      {/* Image */}
+      <div className="card-image-container">
+        <img src={image} alt={name} className="card-image" />
+
+        {/* Gradient overlay */}
+        <div className="image-overlay"></div>
+
+        {/* Delivery time badge */}
+        <span className="time-badge">{time} mins</span>
+      </div>
+
+      {/* Content */}
+      <div className="card-content">
+        <h3 className="restaurant-name">{name}</h3>
+
+        <div className="card-info">
+          <span className="rating">
+            <FaStar /> {rating}
+          </span>
+          <span className="dot">•</span>
+          <span>{time} mins</span>
+        </div>
+
+        <p className="cuisine">{cuisine}</p>
+        <p className="location">{location}</p>
+      </div>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="app">
