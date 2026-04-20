eadb319883522e3c289f16dc22469928af57c3f5
rahoolkar <batman30jan2002@gmail.com>
2026-04-20 20:21:32 +0530

    state variables

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import restaurantList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 function Body() {
+  const [restaurantListCards, setRestaurantListCards] =
+    useState(restaurantList);
+
+  function filterTopRatedRestaursnts() {
+    const filteredRestaurants = restaurantListCards.filter((restaurant) => {
+      return restaurant.info.avgRating > 4.5;
+    });
+    setRestaurantListCards(filteredRestaurants);
+  }
+
   return (
     <div className="body">
-      <div className="restaunrant-card-filter"></div>
+      <div className="restaunrant-card-filter">
+        <button
+          className="top-rated-filter-btn"
+          onClick={filterTopRatedRestaursnts}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurant-card-container">
-        {restaurantList.map((restaurant, index) => {
+        {restaurantListCards.map((restaurant, index) => {
           return (
             <RestaurantCard
               key={restaurant.info.id}
